c677bdd3607489151654c8289aea05ba031eea56
swapnilgoyalinfotech <swapnil@goyalinfotech.com>
2026-07-14 17:07:01 +0530

    test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    initAos();
+  }, []);
   return <Component {...pageProps} />;
 }
+
+const initAos = async () => {
+  await import("aos/dist/aos.css");
+  const Aos = (await import("aos")).default;
+  Aos.init({
+    duration: 1000,
+    once: true,
+  });
+};
