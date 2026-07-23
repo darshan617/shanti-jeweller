a352c0cc910ee542d177fd5d5442a4323ff4c177
Saif shaikh <saif@goyalinfotech.com>
2026-07-23 17:20:36 +0530

    created navbar, banner, and signature collection components

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import Banner from '@/components/home/banner/Banner'
+import Layout from '@/components/Layout/Layout'
+import React from 'react'
+
+const index = () => {
+  return (
+    <Layout> 
+        <Banner />
+    </Layout>
+  )
+}
+
+export default index
